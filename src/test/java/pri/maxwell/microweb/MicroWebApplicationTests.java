9c154d2921d4fb709e9c77efd2eca214aad7294f
maxwell <mjc788999@foxmail.com>
2021-10-13 16:35:58 +0800

    init: 初始化 microWeb 项目

---
+++ src/test/java/pri/maxwell/microweb/MicroWebApplicationTests.java
@@ -0,0 +1,13 @@
+package pri.maxwell.microweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
